a9add9ccedcf526631bc7d22c9caf8f0e2f942b8
Nathalynas <nathaly20camargo@gmail.com>
2025-05-22 14:38:45 -0300

    imagem home

--- crud-plantas/app/Views/templates/footer.php
+++ crud-plantas/app/Views/templates/footer.php
@@ -1,8 +1,13 @@
     </div>
+    
     <footer class="text-center mt-5 mb-3 text-muted">
         <p>&copy; <?= date('Y') ?> - Sistema de Cadastro de Plantas Ornamentais</p>
     </footer>
+
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
+    <img src="<?= base_url('assets/img/home.png') ?>" alt="Minha Planta" class="img-fluid" style="max-width: 200px;">
+    
 </body>
 </html>
