2a2ae6003fcf712d120b7805aee0bb6d017e1918
Nathalynas <nathaly20camargo@gmail.com>
2025-05-28 14:11:21 -0300

    ajuste footer

--- crud-plantas/app/Views/templates/footer.php
+++ crud-plantas/app/Views/templates/footer.php
@@ -1,8 +1,10 @@
 </div>
 
-<footer class="text-center mt-5 mb-3 text-muted">
-    <p>&copy; <?= date('Y') ?> - Sistema de Cadastro de Plantas Ornamentais</p>
-</footer>
+<?php if (isset($isHomePage) && $isHomePage === true): ?>
+    <footer class="text-center mt-5 mb-3 text-muted">
+        <p>&copy; <?= date('Y') ?> - Sistema de Cadastro de Plantas Ornamentais</p>
+    </footer>
+<?php endif; ?>
 
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
